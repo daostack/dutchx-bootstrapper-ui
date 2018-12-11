ad430f2cde56bcf7630e811676f559687bfa7914
Doug Kent <github@douglaskent.com>
2018-12-11 13:04:45 -0500

    fix runTimerAtDate compile error

--- src/organizations/dashboard.ts
+++ src/organizations/dashboard.ts
@@ -208,13 +208,13 @@ export class Dashboard {
       await UtilsInternal.increaseTime(100000000000, this.web3.web3);
     }
 
-    UtilsInternal.runTimerAtDate(this.lockingPeriodEndDate, () => {
+    UtilsInternal.runTimerAtDate(msUntilCanRedeem, () => {
       this.canRedeem = true;
       if (this.org) {
         this.computeRedeemables();
       }
       // $('#globalRedeemBtn').addClass('enabled');
-    }), msUntilCanRedeem);
+    });
 
     const dashboard = $(this.dashboardElement);
 
--- src/services/utils.ts
+++ src/services/utils.ts
@@ -54,12 +54,11 @@ export class Utils {
    * @param date 
    * @param func 
    */
-  public static runTimerAtDate(date, func) {
+  public static runTimerAtDate(msUntil, func) {
     var now = (new Date()).getTime();
-    var then = date.getTime();
-    var diff = Math.max((then - now), 0);
+    var diff = Math.max((msUntil - now), 0);
     if (diff > 0x7FFFFFFF) //setTimeout limit is MAX_INT32=(2^31-1)
-      setTimeout(() => { Utils.runTimerAtDate(date, func); }, 0x7FFFFFFF);
+      setTimeout(() => { Utils.runTimerAtDate(msUntil, func); }, 0x7FFFFFFF);
     else
       setTimeout(func, diff);
   }
