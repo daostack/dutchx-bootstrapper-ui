ccb7ff1c589c50dd52d30d8d23d1a1fa50154a11
Doug Kent <github@douglaskent.com>
2019-02-03 07:10:21 -0600

    disable button while popover is visibile

--- src/services/balloonService.ts
+++ src/services/balloonService.ts
@@ -3,43 +3,61 @@ import { EventMessageType } from '../entities/GeneralEvents';
 
 export class BalloonService {
 
-  public static show(config: IBalloonConfig) {
+  public static show(config: IBalloonConfig): Promise<void> {
+
     const element = $(config.originatingUiElement);
-    if ($(element).data('bs.popover') && $($(element).data('bs.popover').tip).hasClass('show')) {
+
+    if (this.getPopover(element)) {
       return;
     }
 
-    // tslint:disable-next-line: max-line-length
-    const title = `<div class="balloonTitle">An Error Has Occurred <button type="button" class="closeBalloon" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>`;
-    const content = `<div class="balloonBody">${config.content}</div>`;
+    return new Promise((resolve: () => void) => {
 
-    const po = element.popover(
-      Object.assign({},
-        config as PopoverOption,
-        {
-          container: 'body',
-          html: true,
-          placement: 'top',
-          title,
-          trigger: 'manual',
-        },
-        { content }));
+      // tslint:disable-next-line: max-line-length
+      const title = `<div class="balloonTitle">An Error Has Occurred <button type="button" class="closeBalloon" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>`;
+      const content = `<div class="balloonBody">${config.content}</div>`;
 
-    $(element).on('inserted.bs.popover', (_evt: Event): void => {
-      $($(_evt.target).data('bs.popover').tip).addClass('balloon');
+      element.popover(
+        Object.assign({},
+          config as PopoverOption,
+          {
+            container: 'body',
+            html: true,
+            placement: 'top',
+            title,
+            trigger: 'manual',
+          },
+          { content }));
+
+      element.on('inserted.bs.popover', (_evt): void => {
+        const popover = this.getPopover(element);
+        popover.addClass('balloon');
+        popover.data(this.promiseResolveFn, resolve);
+      });
+
+      setTimeout(() => {
+        element.popover('show');
+      }, 0);
     });
-
-    // setTimeout(() => { element.triggerHandler('focus'); }, 0);
-    // setTimeout(() => { element.focus(); }, 0);
-    setTimeout(() => {
-      element.popover('show');
-    }, 0);
   }
 
+  public static hide(element: JQuery) {
+    const popover = element.parents('.popover');
+    // resolve the promise
+    (popover.data(this.promiseResolveFn) as () => void)();
+    popover.data(this.promiseResolveFn, undefined);
+    popover.popover('dispose');
+  }
+
+  private static promiseResolveFn = 'balloon.promise.resolve';
+
+  private static getPopover(element: JQuery): JQuery {
+    return element.data('bs.popover') ? $(element.data('bs.popover').tip) : undefined;
+  }
 }
 
 $(document).on('click', '.popover .closeBalloon', function() {
-  $(this).parents('.popover').popover('dispose');
+  BalloonService.hide($(this));
 });
 
 export interface IBalloonConfig {
