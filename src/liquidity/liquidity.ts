18e3255cead344fbd0a915110736025be32541e7
Doug Kent <github@douglaskent.com>
2019-04-19 14:44:01 +0200

    refactor DAO loading.  finish token liquidity

--- src/liquidity/liquidity.ts
+++ src/liquidity/liquidity.ts
@@ -1,44 +1,77 @@
 import { AureliaConfiguration } from 'aurelia-configuration';
+import { EventAggregator } from 'aurelia-event-aggregator';
 import { autoinject } from 'aurelia-framework';
+import { BaseNetworkPage } from 'baseNetworkPage';
 import { SchemeInfo } from 'entities/SchemeInfo';
+import { loadavg } from 'os';
+import { DaoService } from 'services/DaoService';
 import { ITokenSpecification, LockService } from 'services/lockServices';
+import { NetworkConnectionWizards } from 'services/networkConnectionWizards';
+import { SchemeService } from 'services/SchemeService';
 import { SortService } from 'services/SortService';
 import { TokenService } from 'services/TokenService';
-import { Dashboard } from '../organizations/dashboard';
+import { BigNumber, Web3Service } from 'services/Web3Service';
 import {
-  LockingToken4ReputationWrapper, WrapperService
+  ArcService, LockingToken4ReputationWrapper, WrapperService
 } from '../services/ArcService';
 
 @autoinject
-export class Liquidity {
+export class Liquidity extends BaseNetworkPage {
   protected lockService: LockService;
   private noScheme: boolean = false;
   private tokens: Array<ITokenSpecificationX>;
-  private loading = true;
 
   constructor(
-    private appConfig: AureliaConfiguration,
+    web3: Web3Service,
+    daoService: DaoService,
+    schemeService: SchemeService,
+    appConfig: AureliaConfiguration,
     private tokenService: TokenService,
-    private dashboard: Dashboard
+    eventAggregator: EventAggregator,
+    networkConnectionWizards: NetworkConnectionWizards,
+    arcService: ArcService,
+    web3Service: Web3Service
   ) {
+    super(
+      web3,
+      schemeService,
+      daoService,
+      eventAggregator,
+      appConfig,
+      networkConnectionWizards,
+      arcService,
+      web3Service);
+  }
+
+  public async activate(options: any = {}): Promise<void> {
+
+    this.subscriptions.push(this.eventAggregator
+      .subscribe('DAO.loaded', () => this.load()));
+
+    return super.activate(options);
   }
 
   public async attached() {
     $('body').css('overflow-y', 'auto');
+  }
 
+  public async load() {
+    this.loading = true;
     let schemeInfo: SchemeInfo;
     /**
      * presumes the Dashboard page has previously been loaded
      */
     try {
-      schemeInfo = this.dashboard.getSchemeInfoFromName('LockingToken4Reputation');
+      schemeInfo = this.getSchemeInfoFromName('LockingToken4Reputation');
       const tokenLockingWrapper = await WrapperService.factories.LockingToken4Reputation.at(schemeInfo.address);
 
       // make a copy because the original is used by the token locking dashboard
       this.tokens = [...this.appConfig.get('lockableTokens')];
 
       for (const tokenSpec of this.tokens) {
-        tokenSpec.isLiquid = await this.tokenService.getTokenIsLiquid(tokenSpec.address, tokenLockingWrapper);
+        const price = await this.tokenService.getTokenPriceFactor(tokenSpec.address, tokenLockingWrapper);
+        tokenSpec.isLiquid = !!price;
+        tokenSpec.price = price;
       }
 
       this.tokens = [
@@ -67,4 +100,5 @@ export class Liquidity {
 
 interface ITokenSpecificationX extends ITokenSpecification {
   isLiquid: boolean;
+  price: BigNumber;
 }
