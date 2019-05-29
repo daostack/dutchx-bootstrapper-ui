cc40404d503ce01b3cda72a167f25670953995ce
Alex <a@alexzak.me>
2019-05-30 01:01:25 +0300

    Revert "remove more packages"
    This reverts commit b236c083f46e38a73829fe072b38d3285461b2df.

---
+++ test/unit/app.spec.ts
@@ -0,0 +1,62 @@
+import { EventAggregator } from 'aurelia-event-aggregator';
+import { PLATFORM } from 'aurelia-pal';
+import { BindingSignaler } from 'aurelia-templating-resources';
+import { App } from '../../src/app';
+
+class RouterStub {
+  public routes;
+
+  public options: any = {};
+
+  public configure(handler) {
+    handler(this);
+  }
+
+  public map(routes) {
+    this.routes = routes;
+  }
+
+  public fallbackRoute(_route: string) { return null; }
+}
+
+describe('the App module', () => {
+  let app: App;
+  let mockedRouter: any;
+
+  beforeEach(() => {
+    mockedRouter = new RouterStub();
+
+    const fakeWeb3Service = {
+      defaultAccount: {},
+      isConnected: true,
+    };
+
+    const fakeAppConfigService = {
+      get: (_name: string): string => '',
+    };
+
+    app = new App(
+      fakeWeb3Service as any,
+      {} as BindingSignaler,
+      {} as EventAggregator);
+    (app as any).configureRouter(mockedRouter, mockedRouter);
+  });
+
+  it('contains a router property', () => {
+    expect(app.router).toBeDefined();
+  });
+
+  it('configures the router title', () => {
+    expect(app.router.title).toEqual('dxDAO');
+  });
+
+  it('should have a dashboard route', () => {
+    expect(app.router.routes).toContainEqual({
+      moduleId: PLATFORM.moduleName('./landing'),
+      name: 'landing',
+      nav: false,
+      route: ['', '/', 'landing', 'home'],
+      title: '',
+    });
+  });
+});
---
+++ test/unit/home.spec.ts
@@ -0,0 +1,35 @@
+import { bootstrap } from 'aurelia-bootstrapper';
+import { PLATFORM } from 'aurelia-pal';
+import { StageComponent } from 'aurelia-testing';
+
+describe('Home Page', () => {
+  // const component;
+
+  // beforeEach(async () => {
+  //   component = StageComponent
+  //     .withResources('home')
+  //     .inView('<home></home>');
+  //   await component.create(bootstrap);
+  // });
+
+  // // only jest supports creating snapshot:
+  // if (jest) {
+  //   it('should render correctly', () => {
+  //     expect(document.body.outerHTML).toMatchSnapshot();
+  //   });
+  // }
+
+  // it('should render heading', () => {
+  //   const headerElement = document.querySelector('h1') as HTMLElement;
+  //   expect(headerElement).not.toBeFalsy();
+  //   expect(headerElement.innerHTML).toBe('Creating DAOs for a Collective Intelligence');
+  // });
+
+  it('stub', () => {
+    expect(true);
+  });
+
+  // afterEach(() => {
+  //   component.dispose();
+  // });
+});
