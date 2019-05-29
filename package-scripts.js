7d1e7b582ec306c23de0d7163eff35311ee29ef5
Alex <a@alexzak.me>
2019-05-30 01:01:25 +0300

    Revert "Revert "remove more packages""
    This reverts commit cc40404d503ce01b3cda72a167f25670953995ce.

--- package-scripts.js
+++ package-scripts.js
@@ -4,7 +4,6 @@ const {
   rimraf
 } = require("nps-utils");
 
-const { config: { port: E2E_PORT } } = require("./test/protractor.conf");
 const lintCommand = "tslint {args} -c tslint.json 'src/**/*.ts' && tslint -c tslint.json 'test/**/*.ts' && tslint -c tslint.json 'custom_typings/**/*.ts'";
 
 module.exports = {
@@ -15,50 +14,6 @@ module.exports = {
     lint: {
       default: lintCommand.replace('{args}',''),
       andFix: lintCommand.replace('{args}','--fix')
-    },
-    test: {
-      default: "nps test.jest",
-      jest: {
-        default: "jest",
-        coverage: rimraf("test/coverage-jest"),
-        accept: "jest -u",
-        watch: "jest --watch",
-        updateSnapshots: "jest --updateSnapshot"
-      },
-      karma: {
-        default: series(
-          rimraf("test/coverage-karma"),
-          "karma start test/karma.conf.js"
-        ),
-        watch: "karma start test/karma.conf.js --auto-watch --no-single-run",
-        debug: "karma start test/karma.conf.js --auto-watch --no-single-run --debug"
-      },
-      all: concurrent({
-        browser: series.nps("test.karma", "e2e"),
-        jest: "nps test.jest"
-      })
-    },
-    e2e: {
-      default:
-        concurrent({
-          webpack: `webpack-dev-server --inline --port=${E2E_PORT}`,
-          protractor: "nps e2e.whenReady"
-        }) + " --kill-others --success first",
-      protractor: {
-        install: "webdriver-manager update",
-        default: series(
-          "nps e2e.protractor.install",
-          "protractor test/protractor.conf.js"
-        ),
-        debug: series(
-          "nps e2e.protractor.install",
-          "protractor test/protractor.conf.js --elementExplorer"
-        )
-      },
-      whenReady: series(
-        `wait-on --timeout 120000 http-get://localhost:${E2E_PORT}/index.html`,
-        "nps e2e.protractor"
-      )
     },
     build: {
       production: {
